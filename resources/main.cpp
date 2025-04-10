b6fe560ad4c4e828aa91f101c3dc5ecfb0ce46d0
Brody Grissom <bgriss11@gmail.com>
2025-04-10 08:20:34 -0400

    Added preliminary TerrainParser code to main file

--- resources/main.cpp
+++ resources/main.cpp
@@ -3,5 +3,17 @@
 
 int main() {
     std::cout << "Glacier Guides build is successful!" << std::endl;
+
+    TerrainParser terrainParser;
+    if (terrainParser.loadFile("map.osm"))
+    {
+        std::cout << "TerrainParser parsed" << terrainParser.getPoints().size() << " points from OpenSkiMap." << std::endl;
+        std::cout << "TerrainParser parsed" << terrainParser.getTrails().size() << " trails from OpenSkiMap." << std::endl;
+    }
+    else
+    {
+        std::cerr << "TerrainParser failed to parse the OSM file from OpenSkiMap" << std::endl;
+    }
+
     return 0;
 }
