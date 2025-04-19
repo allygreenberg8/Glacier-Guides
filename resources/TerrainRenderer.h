1a00fe49c7538280d744c83f15499a17b992e669
Brody Grissom <139830530+bgriss11@users.noreply.github.com>
2025-04-19 14:55:41 -0400

    Update TerrainRenderer.h

--- resources/TerrainRenderer.h
+++ resources/TerrainRenderer.h
@@ -13,34 +13,34 @@
 class TerrainRenderer
 {
 private:
-    // Function will take the elevation and path data from the parser function
+    // Takes the elevation and path data from the parser function
     void loadTerrain();
 
-    // Function draws in shading to make map look 3D
+    // Draws in shading to make map look 3D
     void drawShades(sf::RenderWindow& mapWindow);
 
-    // Function will draw in the red line path between the chosen starting point and ending point
+    // Draws in the red line path between the chosen starting point and ending point
     void drawPathLine(sf::RenderWindow& mapWindow);
 
-    // Variable stores a reference to the parsed data from the parser function in TerrainRenderer.cpp
+    // Stores a reference to the parsed data from the parser function in TerrainRenderer.cpp
     const TerrainParser& terrainParser;
 
-    // Variable sotres a 2D vector of the elevations of the selected terrain
+    // Stores a 2D vector of the elevations of the selected terrain
     std::vector<std::vector<double>> elevationData;
 
-    // Variable stores the start and end points of the chosen pathy
+    // Stores the start and end points of the chosen pathy
     std::vector<int> pathPoints;
 
-    // Variable stores the width of the map screen that will display the terrain
+    // Stores the width of the map screen that will display the terrain
     int terrainWidth;
     
-    // Variable stores the height of the map screen that will display the terrain
+    // Stores the height of the map screen that will display the terrain
     int terrainHeight;
 
-    // Variable stores the minimum elevation given for path
+    // Stores the minimum elevation given for path
     double minElevation;
 
-    // Variable stores the user-input of maximum elevation chosen for path
+    // Stores the user-input of maximum elevation chosen for path
     double maxElevation;
 
 
@@ -48,10 +48,10 @@ public:
     // Constructor takes in parsed elevation data from my TerrainParser function in that file
     TerrainRenderer(const TerrainParser& terrainParser);
 
-    // Function draws terrain and the chosen paths to the display window
+    // Draws terrain and the chosen paths to the display window
     void renderTerrain(sf::RenderWindow& mapWindow);
 
-    // Function takes in path calculated by Dijsktra or A* so it can be drawn into map screen
+    // Takes in path calculated by Dijsktra or A* so it can be drawn into map screen
     void choosePath(const std::vector<int>& path);
 
 };
